56974c376f08d3838433dc6e56d716ae68d2da42
Vdell <van2557@email.vccs.edu>
2023-02-03 20:57:35 -0500

    Commit to assignment

--- Lab 03 - Draw Using Functions/lab_03.py
+++ Lab 03 - Draw Using Functions/lab_03.py
@@ -7,19 +7,26 @@ SCREEN_HEIGHT = 600
 arcade.open_window(SCREEN_WIDTH, SCREEN_HEIGHT, "Drawing with Functions")
 arcade.set_background_color(arcade.color.DARK_BLUE)
 arcade.start_render()
+def draw_pizza(x,y):
+    #draw the crust
+    arcade.draw_rectangle_filled(x,y,500,50, (115,58,58))
+    #draw the base with cheese
+    arcade.draw_triangle_filled(x-250,y-25,x+250,y-25,x,y-500, (247,208,52))
+    #draw the pepperoni
+    arcade.draw_circle_filled(x-100,y-125,50, (178,13,46))
+    arcade.draw_circle_filled(x+100,y-125,50, (178,13,46))
+    arcade.draw_circle_filled(x,y-215,50, (178,13,46))
+    arcade.draw_circle_filled(x,y-350,50, (178,13,46))
 
-#draw the crust
-arcade.draw_rectangle_filled(300,500,500,50, (115,58,58))
 
-#draw the base with cheese
-arcade.draw_triangle_filled(50,475,550,475,300,25, (247,208,52))
+def on_draw(delta_time):
+    arcade.start_render()
+    draw_pizza(on_draw.pizza1_x,495)
 
-#draw the pepperoni
-arcade.draw_circle_filled(200,400,50, (178,13,46))
-arcade.draw_circle_filled(400,400,50, (178,13,46))
-arcade.draw_circle_filled(300,300,50, (178,13,46))
-arcade.draw_circle_filled(300,170,50, (178,13,46))
+    on_draw.pizza1_x += 10
+
+on_draw.pizza1_x=-300
+arcade.schedule(on_draw, 1/60)
 
 #  Finish and run
-arcade.finish_render()
 arcade.run()
