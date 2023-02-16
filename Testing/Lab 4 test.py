86d29989fb1f4f30eb33c7c47fb5d449235a8e4f
Vdell <van2557@email.vccs.edu>
2023-02-16 11:28:25 -0500

    Commit to assignment

--- Testing/Lab 4 test.py
+++ Testing/Lab 4 test.py
@@ -1,4 +1,4 @@
-import random
+import arcade
 def main():
     print()
     print("Welcome to Camel!")
@@ -27,30 +27,12 @@ while not done:
 user_choice = input("What is your choice? ")
 if user_choice.upper() == "Q":
     print ("Get outta here!")
-    done = True
+    break
+
 elif user_choice.upper() == "E":
     print("You have traveled " + str(miles_traveled) + " miles.")
     print("You have " + str(canteen) + " drinks left.")
     print("The natives are " + str(miles_traveled - natives_distance) + " miles away.")
-elif user_choice.upper()== "D":
-    print("You have decided to rest for the night.")
-    print("Your camel is resting and happy! :)")
-    print("The natives are " + str(random.randrange(7,14) + "miles away."))
-elif user_choice.upper() == "C":
-    print("You have chosen to continue at full speed.")
-    print("You have traveled " + str(random.randrange(10,20) + " miles."))
-    thirst = thirst + 1
-    camel_tiredness = camel_tiredness + random.randrange(1,3)
-    natives_distance = natives_distance + random.randrange(7,14)
-elif user_choice.upper() == "B":
-    print("You go ahead at moderate speed.")
-    print("You have traveled " + str(miles_traveled + random.randrange(5,12) + " miles."))
-    thirst = thirst + 1
-    camel_tiredness = camel_tiredness + 1
-elif user_choice.upper() == "A":
-    print("You chose to drink from your canteen.")
-
-
 
 
 
