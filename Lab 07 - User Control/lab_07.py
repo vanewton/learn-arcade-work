f71a8dc625f0d749cb4b5ad7b363946019ddb2f6
Vdell <van2557@email.vccs.edu>
2023-03-23 23:13:13 -0400

    Commit to assignment

--- Lab 07 - User Control/lab_07.py
+++ Lab 07 - User Control/lab_07.py
@@ -6,23 +6,39 @@ import arcade
 SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 600
 
+class Pizza:
+    def on_draw(self):
+        arcade.start_render()
+        self.on_draw()
 
+
+    def draw_pizza(x, y):
+        # draw the crust
+        arcade.draw_rectangle_filled(x, y, 500, 50, (115, 58, 58))
+        # draw the base with cheese
+        arcade.draw_triangle_filled(x - 250, y - 25, x + 250, y - 25, x, y - 500, (247, 208, 52))
+        # draw the pepperoni
+        arcade.draw_circle_filled(x - 100, y - 125, 50, (178, 13, 46))
+        arcade.draw_circle_filled(x + 100, y - 125, 50, (178, 13, 46))
+        arcade.draw_circle_filled(x, y - 215, 50, (178, 13, 46))
+        arcade.draw_circle_filled(x, y - 350, 50, (178, 13, 46))
+
+        self.pizza.draw = Pizza(50,51,50)
 class MyGame(arcade.Window):
     """ Our Custom Window Class"""
-
     def __init__(self):
         """ Initializer """
 
         # Call the parent class initializer
         super().__init__(SCREEN_WIDTH, SCREEN_HEIGHT, "Lab 7 - User Control")
 
-    def on_draw(self):
-        arcade.start_render()
+
 
 
 def main():
     window = MyGame()
     arcade.run()
+
 
 
 main()
